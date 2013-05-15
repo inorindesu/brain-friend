05199b75f3a1489767beccdf07c288728a6ec379
Inori Sakura <inorindesu@gmail.com>
2013-05-16 00:08:06 +0800

    Dump script was reinforced.
    Human-readable opcodes and opcode values will be written to output
    instead of opcode values only.

--- scripts/dump.lua
+++ scripts/dump.lua
@@ -4,7 +4,19 @@ function init()
 end
 
 function receiveCode(opCode, param)
-   writeLine("Opcode: " .. opCode .. ", Parameter: " .. param)
+   opCodeString = ""
+   if     opCode == Opcodes.Nop then opCodeString = "nop"
+   elseif opCode == Opcodes.Add then opCodeString = "add"
+   elseif opCode == Opcodes.Next then opCodeString = "next"
+   elseif opCode == Opcodes.Bz then opCodeString = "bz"
+   elseif opCode == Opcodes.B then opCodeString = "b"
+   elseif opCode == Opcodes.In then opCodeString = "in"
+   elseif opCode == Opcodes.Out then opCodeString = "out"
+   elseif opCode == Opcodes.Init then opCodeString = "init"
+   elseif opCode == Opcodes.Dumpstat then opCodeString = "dumpstat"
+   else opCodeString = "(N/A)"
+   end
+   writeLine("Opcode: " .. opCodeString .. "(" .. opCode  .. "), Parameter: " .. param)
 end
 
 function atEnd()
